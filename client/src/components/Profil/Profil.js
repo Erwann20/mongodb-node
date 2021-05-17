a4eb15dc7aa69d00978f5d3574ffa7d88ad1a98c
matisse.99@hotmail.fr <matisse.99@hotmail.fr>
2021-05-17 17:45:01 +0200

    page Profile base Les bases des pages profils   flex-flow: column nowrap;   align-items: center;   justify-content: center;

--- client/src/components/Profil/Profil.js
+++ client/src/components/Profil/Profil.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import "./Profil.css"
 
 const Profil = () => {
     return(
-            <div>
-                Profil
-            </div>
+        <div className="pageProfil">
+            <h2>Page Profil</h2>
+            <h3>Prénom : JEAND-EDOUARD</h3>
+            <h3>Nom : ADOUARD</h3>
+            <h3>
+                Ville : Pimpousse-les-bains, France
+            </h3>
+            <h3>Passion : J'AIME LE FROMAGE</h3>
+            <button className="messageButton">Envoyer un message</button>
+        </div>
     )
 }
 
